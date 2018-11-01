4ffe0c60eca9def685ccf01ffb2c6a42ef203fa4
kingmak <kingmak@hotmail.com>
2018-11-01 14:01:07 -0500

    add plaintext test file, add encrypted test file, add file read and write funcs

--- maincode.py
+++ maincode.py
@@ -4,6 +4,14 @@ from crypt import Crypt
 if len(sys.argv) != 3:
     sys.exit("usage: {} file_to_encrypt password".format(sys.argv[0]))
 
+def encout(filename, data):
+    with open(filename, 'w') as outfile:
+        outfile.write(data)
+
+def decin(filename):
+    data = open(filename).read()
+    return data
+
 ####################################################################################################
 ## file read
 ####################################################################################################
@@ -14,8 +22,12 @@ msg     = bytes(fileobj.read(), "utf-8")
 ccc = Crypt()
 key = bytes(sys.argv[1], "utf-8")
 
-encrypted = ccc.encrypt(key, msg)    
-decrypted = ccc.decrypt(key, encrypted)
+#encrypted = ccc.encrypt(key, msg)
+#decrypted = ccc.decrypt(key, encrypted)
 
-print(encrypted)
+#encout('enc_' + sys.argv[1], encrypted)
+fileenc   = decin('enc_test')
+decrypted = ccc.decrypt(key, fileenc)
+
+#print(encrypted)
 print(decrypted)

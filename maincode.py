8c7e06c2e36681823c9802c3fbff69c272cd8979
kingmak <kingmak@hotmail.com>
2018-11-01 00:53:36 -0500

    fix maincode.py

--- maincode.py
+++ maincode.py
@@ -1,19 +1,18 @@
 import sys
 from crypt import Crypt
 
-#if len(sys.argv) != 3:
-#    sys.exit('usage: {} file_to_encrypt password'.format(sys.argv[0]))
+if len(sys.argv) != 3:
+    sys.exit("usage: {} file_to_encrypt password".format(sys.argv[0]))
 
 ####################################################################################################
 ## file read
 ####################################################################################################
 fileobj = open(sys.argv[1])
-lines   = fileobj.read()
+msg     = bytes(fileobj.read(), "utf-8")
 ####################################################################################################
 
 ccc = Crypt()
-key = b"ahmed"
-msg = b"hammad"
+key = bytes(sys.argv[1], "utf-8")
 
 encrypted = ccc.encrypt(key, msg)    
 decrypted = ccc.decrypt(key, encrypted)
